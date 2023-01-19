1f8fbc8f8c2340fd579c9482e3ec03f7d8fceaa7
Gambit142 <nzefrancismaryeugorji@gmail.com>
2023-01-19 12:35:56 +0100

    Link to search page after log in is successful

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || long_urls_path
+    stored_location_for(resource) || articles_path
   end
 
   def update_allowed_parameters
