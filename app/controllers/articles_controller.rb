8990d55048e7f7dfebd4e19f355f8510f51cce7c
Gambit142 <nzefrancismaryeugorji@gmail.com>
2023-01-19 12:36:37 +0100

    Create actions to control user requests in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,10 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def list
+    @articles = Article.all.order(params[:column] => params[:direction])
+    render(partial: 'articles', locals: { articles: @articles })
+  end
 end
