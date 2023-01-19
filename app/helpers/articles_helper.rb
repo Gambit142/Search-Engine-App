dbc0bf8756a10074aa4d192f9253fc73a4357e0d
Gambit142 <nzefrancismaryeugorji@gmail.com>
2023-01-19 12:37:06 +0100

    Create helpers that will make sorting easy

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,13 @@
 module ArticlesHelper
+  def next_direction
+    params[:direction] == 'asc' ? 'desc' : 'asc'
+  end
+
+  def sort_link(column:, label:)
+    link_to(label, list_articles_path(column: column, direction: next_direction))
+  end
+
+  def sort_indicator
+    tag.span(class: "sort sort-#{params[:direction]}")
+  end
 end
